19ed14a5cf033af4ee1502c9ffb3d9fc88568d10
Graziela de Oliveira <grazielaoliveiragg@gmail.com>
2023-05-29 12:53:07 +0000

    python

---
+++ 1Lista.py
@@ -0,0 +1,11 @@
+#Soma, Subtração, Multiplicação e Divisão de dois números
+
+n1 = float (input('Informe o primeiro número para realizar as operações: '))
+n2 = float (input('Informe o segundo número para realizar as operações: '))
+
+soma = n1 + n2
+subtracao = n1 - n2
+divisao = n1/n2
+mult = n1*n2
+
+print ('Soma: ', soma, ', Subtração: ', subtracao, ', Divisão: ', divisao, ', Multiplicação: ', mult,)
